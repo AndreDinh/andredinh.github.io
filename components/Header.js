6e48bb80e83ed9d9b197d6c5ecb8b0fbebc4ac16
Andre Dinh <dinh.andre@yahoo.com>
2019-05-14 17:18:37 -0700

    updated profile added images

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,10 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Andre Dinh</h1>
+                <p>Front-End Developer<br />
+                Photographer and Powerlifter
+                </p>
             </div>
         </div>
         <nav>
@@ -20,7 +21,7 @@ const Header = (props) => (
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li> */}
             </ul>
         </nav>
     </header>
--- components/Main.js
+++ components/Main.js
@@ -8,35 +8,73 @@ import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 class Main extends React.Component {
   render() {
 
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = <div className="close" onClick={() => { this.props.onCloseArticle() }}></div>
 
     return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+      <div id="main" style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Intro</h2>
           <span className="image main"><img src="/static/images/pic01.jpg" alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <p>Welcome to my website. I been doing web developing since the earlier 2000s but got serious in Front-End Development 2015. By the way, check out my <a href="#work">awesome work</a>.</p>
+          <p></p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Work</h2>
-          <span className="image main"><img src="/static/images/pic02.jpg" alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <span className="image main"><img src="/static/images/att.png" alt="" /></span>
+          <p>Currently maintaining <a href="https://www.att.com/">att.com</a><br></br> <strong>Buy-Flow</strong>
+          <br />
+            Managed on high-profile, customer-facing marketing projects for AT&T. Collaborated $2M recovery and redesign project supporting 60k+ users. Worked with product owner, business analyst, and UX design on <a href="https://www.att.com/">att.com</a>
+            <br />
+            Some other pages I worked on:
+            <br />
+            <a href="https://www.att.com/">https://www.att.com/</a> <br />
+            <a href=" https://www.att.com/deals/"> https://www.att.com/deals/</a> <br />
+            <a href="https://www.att.com/wireless/">https://www.att.com/wireless/</a> <br />
+            <a href="https://www.att.com/buy/phones/apple-iphone-xs-max-64gb-silver.html">https://www.att.com/buy/phones/apple-iphone-xs-max-64gb-silver.html</a> <br />
+            <a href="https://www.att.com/plans/connected-car/honda.html">https://www.att.com/plans/connected-car/honda.html</a>
+
+          </p>
+
+          <p>Stack using
+            <ul>
+              <li>
+                AEM(6.1 and 6.4) and Adobe CQ
+              </li>
+              <li>
+                HTML (HTML5)
+              </li>
+              <li>
+                CSS (Responsive web design)
+              </li>
+              <li>
+                JavaScript(Vanilla, Angular, and React)
+              </li>
+
+            </ul>
+          </p>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">About</h2>
-          <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <span className="image main"><img src="/static/images/profile.png" alt="" /></span>
+          <h3>Web Development</h3>
+          <p>I started programing in during the myspace era making profiles and custom curster usinging inline html and css
+          I found passion in coding during jonur year of high school. I made it a goal to become a programer and soon after I went to Bellevue College to be Computer Science major
+          Once I graduated college I realized I love web-develment more than just programing. I went back to school to become a Front-End Develop. Where I spend 3 months intense programing to land my first Front-End developer job
+          I now love what I am doing and love to problem solve new challanges in the technology world
+          </p>
+          <h3> Hobbies</h3>
+          <p>I am a photographer working with friends and family on holidays and events.</p>
+          <span className="image main"><img src="/static/images/pic04.jpg" alt="" /></span>
+          <p>I been a powerlifter for 1 year serious training. Competed and got second in my weight class with over 100 entrance in compention.</p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Contact</h2>
           <form method="post" action="#">
             <div className="field half first">
@@ -57,15 +95,15 @@ class Main extends React.Component {
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a></li>
-            <li><a href="#">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a></li>
-            <li><a href="#">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a></li>
+            {/* <li><a href="#">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a></li> */}
+            {/* <li><a href="#">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a></li> */}
+            {/* <li><a href="#">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a></li> */}
             <li><a href="#">
               <FontAwesomeIcon icon={faGithub} />
             </a></li>
@@ -73,7 +111,7 @@ class Main extends React.Component {
           {close}
         </article>
 
-      </div>
+      </div >
     )
   }
 }
